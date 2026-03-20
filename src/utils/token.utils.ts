34aa99313ac2d170a98dd8668061aa7dfed96956
arieltecay <arieltecay@gmail.com>
2026-03-20 19:21:10 -0300

    Chore: Extend session duration to 365 days

--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -5,13 +5,13 @@ export const generateTokens = (user: IAdminUser) => {
   const accessToken = jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_ACCESS_SECRET!,
-    { expiresIn: '8h' }
+    { expiresIn: '365d' }
   );
 
   const refreshToken = jwt.sign(
     { id: user._id },
     process.env.JWT_REFRESH_SECRET!,
-    { expiresIn: '7d' }
+    { expiresIn: '365d' }
   );
 
   return { accessToken, refreshToken };
